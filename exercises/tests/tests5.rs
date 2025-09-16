d363ce69639cc37971b0ca66362a040bf3ec5dda
Dandelion <1169809321@qq.com>
2025-09-16 20:47:42 +0800

    update

--- exercises/tests/tests5.rs
+++ exercises/tests/tests5.rs
@@ -32,7 +32,9 @@ unsafe fn modify_by_address(address: usize) {
     // code's behavior and the contract of this function. You may use the
     // comment of the test below as your format reference.
     unsafe {
-        todo!("Your code goes here")
+        let r = address as *mut u32;
+        // SAFETY: The `address` is guaranteed to be a valid address
+        *r = 0xAABBCCDD;
     }
 }
 
